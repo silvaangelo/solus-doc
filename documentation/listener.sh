151e5b0aef4f79800c92133a4c6c677228577b6d
Angelo Silva <angelorodriigo.rs@gmail.com>
2018-02-27 19:46:45 -0300

    Troca compilador do listener.sh e adiciona subtitulos da parte de introdução

--- documentation/listener.sh
+++ documentation/listener.sh
@@ -1,7 +1,4 @@
 while :; do
     inotifywait -e modify solus-doc.tex
-    latexmk -pdf solus-doc.tex
-
-    inotifywait -e modify references-solus-doc.bib
-    latexmk -pdf solus-doc.tex
+    pdflatex -pdf solus-doc.tex
 done

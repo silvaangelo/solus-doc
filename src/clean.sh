c2ea33215910c82f8a1a2b26fcfae585fcdf7e91
Angelo Silva <angelorodriigo.rs@gmail.com>
2018-10-24 13:44:10 -0300

    Remoçã ode arquivos não usados

--- src/clean.sh
+++
@@ -1,1 +0,0 @@
-rm *.auxd *.idxd *.lofd *.logd *.lotd *.toc *.aux *.bbl *.blg *.brf *.fls *.fdb_latexmk *.idx *.ilg *.ind *.lof *.log *.lot
--- src/listener.sh
+++
@@ -1,7 +0,0 @@
-while :; do
-    inotifywait -e modify *.tex **/*.tex *.bib
-    latexmk -pdf index.tex
-
-    # Para limpar os logs após compilar, deixe a linha abaixo sem o comentário
-    #./clean.sh
-done

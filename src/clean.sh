8251851daa4aafbd2d877069728927b3919ef83a
Angelo Silva <angelorodriigo.rs@gmail.com>
2018-10-31 00:04:06 -0300

    Remove textual separation and fix technology

---
+++ src/clean.sh
@@ -0,0 +1,1 @@
+rm *.auxd *.idxd *.lofd *.logd *.lotd *.toc *.aux *.bbl *.blg *.brf *.fls *.fdb_latexmk *.idx *.ilg *.ind *.lof *.log *.lot *.out.ps *.dvi .index.tex.swp -f
---
+++ src/listener.sh
@@ -0,0 +1,6 @@
+while :; do
+    inotifywait -e modify *.tex **/*.tex *.bib
+    latexmk -pdf index.tex
+     # Para limpar os logs após compilar, deixe a linha abaixo sem o comentário
+     ./clean.sh
+done
